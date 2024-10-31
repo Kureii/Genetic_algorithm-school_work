faf4216c3210867ebb1b081eb1a51df0ea34d567
kureii (Tomáš Adámek) <ad.tomik@seznam.cz>
2024-10-31 22:51:25 +0100

    add header of float problems

--- python_module/functions_for_python.h
+++ python_module/functions_for_python.h
@@ -6,6 +6,7 @@
 
 #include "../input_structure.h"
 #include "../output_structure.h"
+#include "../real_structures.h"
 
 extern output_structure_t GeneticAlgorithm_MaxOne(input_structure_t input_structure);
 
@@ -14,3 +15,9 @@ extern output_structure_t GeneticAlgorithm_LeadingOne(input_structure_t input_st
 extern output_structure_t GeneticAlgorithm_TradingOne(input_structure_t input_structure);
 
 extern output_structure_t GeneticAlgorithm_MaxZero(input_structure_t input_structure);
+
+extern output_structure_t GeneticAlgorithm_Sphere(input_structure_t input_structure, mapping_structure_t mapping_structure);
+
+extern output_structure_t GeneticAlgorithm_Schwefel(input_structure_t input_structure, mapping_structure_t mapping_structure);
+
+extern output_structure_t GeneticAlgorithm_Rosenbrock(input_structure_t input_structure, mapping_structure_t mapping_structure);
